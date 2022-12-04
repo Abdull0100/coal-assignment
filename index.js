ecd4fac9521a6b2b7b82239dae72620f41ae7106
Jawad <jkabir789@gmail.com>
2022-12-04 10:37:22 +0500

    added few more instructions

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 let instruction;
-
+// max number ah can hold is 255
+// FF in hex = 255 in decimal = 11111111 in binary
 let opCodes = ["mov", "add", "sub", "div", "inc", "dec"];
 let generalRegisters = ["ah", "bh", "ch", "dh", "al", "bl", "cl", "dl"];
-let generalValues = ["0", "3", "0", "0", "0", "0", "0", "0"];
+let generalValues = ["0", "0", "0", "0", "0", "0", "0", "0"];
 let memoryPlaces = [
   "[0h]",
   "[1h]",
@@ -40,6 +41,10 @@ let memoryValues = [
   "0",
 ];
 
+
+
+
+
 function all_value_initial() {
   //for memory places
   for (let i = 0; i < 16; i++) {
@@ -57,9 +62,11 @@ function all_value_initial() {
   for (let i = 0; i < 4; i++) {
     document.getElementById(`${i}L`).innerHTML = generalValues[i + 4];
   }
+
 }
 
 function executer(instruction) {
+
   if (instruction[0] === "mov") {
     if ( // if destination is a register and source is a register
       opCodes.includes(instruction[0]) &&
@@ -71,9 +78,7 @@ function executer(instruction) {
       let dest_index = generalRegisters.indexOf(dest);
       let source_index = generalRegisters.indexOf(source);
       generalValues[dest_index] = generalValues[source_index];
-      document.getElementById(
-        `${Math.floor(dest_index / 4)}${dest_index % 4 == 0 ? "H" : "L"}`
-      ).innerHTML = generalValues[dest_index];
+      all_value_initial();
     } else if ( // if destination is a register and source is a memory place
       opCodes.includes(instruction[0]) &&
       generalRegisters.includes(instruction[1]) &&
@@ -84,9 +89,7 @@ function executer(instruction) {
       let dest_index = generalRegisters.indexOf(dest);
       let source_index = memoryPlaces.indexOf(source);
       generalValues[dest_index] = memoryValues[source_index];
-      document.getElementById(
-        `${Math.floor(dest_index / 4)}${dest_index % 4 == 0 ? "H" : "L"}`
-      ).innerHTML = generalValues[dest_index];
+        all_value_initial();
     } else if ( // if destination is a memory place and source is a register
       opCodes.includes(instruction[0]) &&
       memoryPlaces.includes(instruction[1]) &&
@@ -114,11 +117,165 @@ function executer(instruction) {
         let source = instruction[2];
         let dest_index = generalRegisters.indexOf(dest);
         generalValues[dest_index] = source;
-        document.getElementById(
-            `${Math.floor(dest_index / 4)}${dest_index % 4 == 0 ? "H" : "L"}`
-        ).innerHTML = generalValues[dest_index];
+        toString(generalValues[dest_index]);
+        all_value_initial();
         }
+  } else if (instruction[0] === "add") {
+    if ( // if destination is a register and source is a register
+      opCodes.includes(instruction[0]) &&
+      generalRegisters.includes(instruction[1]) &&
+      generalRegisters.includes(instruction[2])
+    ) {
+      let dest = instruction[1];
+      let source = instruction[2];
+      let dest_index = generalRegisters.indexOf(dest);
+      let source_index = generalRegisters.indexOf(source);
+      generalValues[dest_index] = parseInt(generalValues[dest_index]) + parseInt(generalValues[source_index], 16);
+        all_value_initial();
+    } else if ( // if destination is a register and source is a memory place
+      opCodes.includes(instruction[0]) &&
+      generalRegisters.includes(instruction[1]) &&
+      memoryPlaces.includes(instruction[2])
+    ) {
+      let dest = instruction[1];
+      let source = instruction[2];
+      let dest_index = generalRegisters.indexOf(dest);
+      let source_index = memoryPlaces.indexOf(source);
+      generalValues[dest_index] = parseInt(generalValues[dest_index]) + parseInt(memoryValues[source_index]);
+        all_value_initial();
+    } else if (
+        opCodes.includes(instruction[0]) &&
+        generalRegisters.includes(instruction[1]) &&
+        !isNaN(instruction[2]) //if source is a number
+        ) {
+        let dest = instruction[1];
+        let source = instruction[2];
+        let dest_index = generalRegisters.indexOf(dest);
+        generalValues[dest_index] = parseInt(generalValues[dest_index]) + parseInt(source);
+        all_value_initial();
+        }
+  } else if (instruction[0] === "sub") {
+    if ( // if destination is a register and source is a register
+      opCodes.includes(instruction[0]) &&
+      generalRegisters.includes(instruction[1]) &&
+      generalRegisters.includes(instruction[2])
+    ) {
+      let dest = instruction[1];
+      let source = instruction[2];
+      let dest_index = generalRegisters.indexOf(dest);
+      let source_index = generalRegisters.indexOf(source);
+      generalValues[dest_index] = parseInt(generalValues[dest_index]) - parseInt(generalValues[source_index]);
+        all_value_initial();
+    } else if ( // if destination is a register and source is a memory place
+      opCodes.includes(instruction[0]) &&
+      generalRegisters.includes(instruction[1]) &&
+      memoryPlaces.includes(instruction[2])
+    ) {
+      let dest = instruction[1];
+      let source = instruction[2];
+      let dest_index = generalRegisters.indexOf(dest);
+      let source_index = memoryPlaces.indexOf(source);
+      generalValues[dest_index] = parseInt(generalValues[dest_index]) - parseInt(memoryValues[source_index]);
+        all_value_initial();
+    } else if (
+        opCodes.includes(instruction[0]) &&
+        generalRegisters.includes(instruction[1]) &&
+        !isNaN(instruction[2]) //if source is a number
+        ) {
+        let dest = instruction[1];
+        let source = instruction[2];
+        let dest_index = generalRegisters.indexOf(dest);
+        generalValues[dest_index] = parseInt(generalValues[dest_index]) - parseInt(source);
+        all_value_initial();
+        }
+  } else if (instruction[0] === "mul") {
+    if ( // if destination is a register and source is a register
+      opCodes.includes(instruction[0]) &&
+      generalRegisters.includes(instruction[1]) &&
+      generalRegisters.includes(instruction[2])
+    ) {
+      let dest = instruction[1];
+      let source = instruction[2];
+      let dest_index = generalRegisters.indexOf(dest);
+      let source_index = generalRegisters.indexOf(source);
+      generalValues[dest_index] = parseInt(generalValues[dest_index]) * parseInt(generalValues[source_index]);
+        all_value_initial();
+    } else if ( // if destination is a register and source is a memory place
+      opCodes.includes(instruction[0]) &&
+      generalRegisters.includes(instruction[1]) &&
+      memoryPlaces.includes(instruction[2])
+    ) {
+      let dest = instruction[1];
+      let source = instruction[2];
+      let dest_index = generalRegisters.indexOf(dest);
+      let source_index = memoryPlaces.indexOf(source);
+      generalValues[dest_index] = parseInt(generalValues[dest_index]) * parseInt(memoryValues[source_index]);
+        all_value_initial();
+    } else if (
+        opCodes.includes(instruction[0]) &&
+        generalRegisters.includes(instruction[1]) &&
+        !isNaN(instruction[2]) //if source is a number
+        ) {
+        let dest = instruction[1];
+        let source = instruction[2];
+        let dest_index = generalRegisters.indexOf(dest);
+        generalValues[dest_index] = parseInt(generalValues[dest_index]) * parseInt(source);
+        all_value_initial();
+        }
+  } else if (instruction[0] === "div") {
+    if ( // if destination is a register and source is a register
+      opCodes.includes(instruction[0]) &&
+      generalRegisters.includes(instruction[1]) &&
+      generalRegisters.includes(instruction[2])
+    ) {
+      let dest = instruction[1];
+      let source = instruction[2];
+      let dest_index = generalRegisters.indexOf(dest);
+      let source_index = generalRegisters.indexOf(source);
+      generalValues[dest_index] = parseInt(generalValues[dest_index]) / parseInt(generalValues[source_index]);
+        all_value_initial();
+    } else if ( // if destination is a register and source is a memory place
+      opCodes.includes(instruction[0]) &&
+      generalRegisters.includes(instruction[1]) &&
+      memoryPlaces.includes(instruction[2])
+    ) {
+      let dest = instruction[1];
+      let source = instruction[2];
+      let dest_index = generalRegisters.indexOf(dest);
+      let source_index = memoryPlaces.indexOf(source);
+      generalValues[dest_index] = parseInt(generalValues[dest_index]) / parseInt(memoryValues[source_index]);
+        all_value_initial();
+    } else if (
+        opCodes.includes(instruction[0]) &&
+        generalRegisters.includes(instruction[1]) &&
+        !isNaN(instruction[2]) //if source is a number
+        ) {
+        let dest = instruction[1];
+        let source = instruction[2];
+        let dest_index = generalRegisters.indexOf(dest);
+        generalValues[dest_index] = parseInt(generalValues[dest_index]) / parseInt(source);
+        all_value_initial();
+        }
+  } else if (instruction[0] === "inc") {
+    if ( // if destination is a register and source is a register
+      opCodes.includes(instruction[0]) &&
+      generalRegisters.includes(instruction[1])
+    ) {
+      let dest = instruction[1];
+      let dest_index = generalRegisters.indexOf(dest);
+      generalValues[dest_index] = parseInt(generalValues[dest_index]) + 1;
+        all_value_initial();
+    } else if ( // if destination is a register and source is a memory place
+      opCodes.includes(instruction[0]) &&
+      memoryPlaces.includes(instruction[1])
+    ) {
+      let dest = instruction[1];
+      let dest_index = memoryPlaces.indexOf(dest);
+      memoryValues[dest_index] = parseInt(memoryValues[dest_index]) + 1;
+        all_value_initial();
+    }
   }
+
 }
 
 all_value_initial();
